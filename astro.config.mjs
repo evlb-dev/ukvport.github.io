648ec1077cef55d88df589743e42a798fb4f49bc
evlb-dev <evillalba@icop.edu.ar>
2026-05-14 00:43:18 -0300

    fix asset paths for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -5,6 +6,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://evlb-dev.github.io",
   base: "/ukvport.github.io/",
-
   integrations: [tailwind(), react()],
 });
---
+++ src/utils/assets.ts
@@ -0,0 +1,12 @@
+import astro from "../../public/svg/astro.svg";
+import tailwind from "../../public/svg/tailwindcss.svg";
+import vercel from "../../public/svg/vercel.svg";
+
+import favicon from "../../public/favicon-32x32.png";
+
+export const assets = {
+  astro,
+  tailwind,
+  vercel,
+  favicon,
+};
