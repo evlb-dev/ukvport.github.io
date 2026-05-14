1851b96a5928430166d9fb92712a128f3674bb4f
evlb-dev <evillalba@icop.edu.ar>
2026-05-13 23:59:02 -0300

    fix github pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://evlb-dev.github.io/ukvport.github.io",
+  site: "https://evlb-dev.github.io",
+  base: "/ukvport.github.io/",
+
   integrations: [tailwind(), react()],
 });
